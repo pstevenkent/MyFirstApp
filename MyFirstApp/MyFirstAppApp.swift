e76b963f52640dedd3aba82256582ba9cd8c90d8
pstevenkent <pstevenkent@student.ciputra.ac.id>
2024-04-18 11:29:43 +0700

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Patrick Steven Kent Sugiarto on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
